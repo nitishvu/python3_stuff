0b567e10950cace6a2d1888c9de81bc0ce899163
Nitish vulli <vullinitish@gmail.com>
2020-06-06 12:55:11 +0530

    recursive files print
    Signed-off-by: Nitish vulli <vullinitish@gmail.com>

---
+++ file_ops/print_all_files_recursively.py
@@ -0,0 +1,32 @@
+
+from os import path,listdir,walk
+dir1="/home/nitish/workspace/python3_stuff"
+
+def print_all_files(dir1,full_path):
+    #print(dir1)
+
+    if path.isdir(dir1):
+        print("dir:%s"%dir1)
+        files_list = listdir(dir1)
+        for i in files_list:
+            if '.git' in i:
+                continue
+            if path.isfile(full_path+'/'+i):
+                print("file:%s"%(full_path+'/'+dir1+'/'+i))
+            else:
+                print_all_files(full_path+'/'+i,full_path+'/'+i)
+
+print(dir1)
+print_all_files(dir1,dir1)
+#print(walk(dir1))
+print('*'*50)
+print("Now walk")
+print('*'*50)
+for root,d_names,f_names in walk(dir1):
+	#print (root, d_names, f_names)
+    if not('.git' in root): 
+        print(root)
+    for i in f_names:
+        if '.git' in i or ('.git' in root): 
+            continue
+        print(root+"/"+i)
